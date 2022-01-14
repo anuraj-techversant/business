dcb5f778ee23806b9dcdd114bc538dc8ad90bef2
anuraj-kr <anurajkr23@gmail.com>
2022-01-14 17:23:09 +0530

    alignment correction

--- business/customerapp/views.py
+++ business/customerapp/views.py
@@ -7,6 +7,7 @@ from customerapp.permissions import IsAuthenticatedUser
 from customerapp.models import customer
 from customerapp.serializers import CustomerSerializer
 
+
 class CustomerView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticatedUser]
     serializer_class = CustomerSerializer
@@ -19,8 +20,10 @@ class CustomerView(generics.ListCreateAPIView):
         serializer = CustomerSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": status.HTTP_201_CREATED, "message": "Customer created succesfully", 'data': serializer.data})
-        return Response({"status": status.HTTP_400_BAD_REQUEST, "message": "Customer creation failed", "data": serializer.errors})
+            return Response({"status": status.HTTP_201_CREATED, 
+                "message": "Customer created succesfully", 'data': serializer.data})
+        return Response({"status": status.HTTP_400_BAD_REQUEST, 
+            "message": "Customer creation failed", "data": serializer.errors})
 
 
 class CustomerDescriptionDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -32,7 +35,8 @@ class CustomerDescriptionDetail(generics.RetrieveUpdateDestroyAPIView):
         try:
             details = customer.objects.get(id=pk)
         except:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'message': "Customer detail not found"})
+            return Response({'status': status.HTTP_404_NOT_FOUND, 
+                'message': "Customer detail not found"})
         serializer = CustomerSerializer(details)
         return Response(serializer.data)
 
@@ -41,21 +45,27 @@ class CustomerDescriptionDetail(generics.RetrieveUpdateDestroyAPIView):
         try:
             details = customer.objects.get(id=pk)
         except:
-            return Response({'status': status.HTTP_404_NOT_FOUND, 'message': "Customer detail not found"})
+            return Response({'status': status.HTTP_404_NOT_FOUND, 
+                'message': "Customer detail not found"})
         serializer = CustomerSerializer(details, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'status': status.HTTP_200_OK, 'message': "Customer details updated successfully", 'data': serializer.data})
-        return Response({'status': status.HTTP_400_BAD_REQUEST, 'message': "Customer details updation failed", 'data': serializer.errors})
+            return Response({'status': status.HTTP_200_OK, 
+                'message': "Customer details updated successfully", 'data': serializer.data})
+        return Response({'status': status.HTTP_400_BAD_REQUEST, 
+            'message': "Customer details updation failed", 'data': serializer.errors})
 
     def delete(self, request, pk, format=None):
         try:
             if customer.objects.filter(id=pk).exists():
                 details = customer.objects.get(id=pk)
                 details.delete()
-                return Response({'status': status.HTTP_200_OK, 'message': "Customer detail deleted successfully"})
+                return Response({'status': status.HTTP_200_OK, 
+                    'message': "Customer detail deleted successfully"})
             else:
-                return Response({'status': status.HTTP_404_NOT_FOUND, 'message': "Customer detail not found"})
+                return Response({'status': status.HTTP_404_NOT_FOUND, 
+                    'message': "Customer detail not found"})
         except:
-            return Response({'status': status.HTTP_400_BAD_REQUEST, 'message': "Customer detail deletion failed"})
+            return Response({'status': status.HTTP_400_BAD_REQUEST, 
+                'message': "Customer detail deletion failed"})
 
